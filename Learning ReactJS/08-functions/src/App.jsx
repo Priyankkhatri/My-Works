9f4300d7899f2ca8dd605925d9f9883e99f65647
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-04-01 22:46:12 +0530

    working on functions

---
+++ Learning ReactJS/08-functions/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { Children } from 'react'
+
+const App = () => {
+
+  // function btnClicked() {
+  //   console.log("Button is clicked");
+  // }
+
+  // function mouseEnter() {
+  //   console.log("Mouse Entered");
+  // }
+  // function mouseLeave() {
+  //   console.log("Mouse leave");
+  // }
+
+    function inputChanging(val){
+        console.log(val);
+    }
+
+  return (
+    <div>
+      <h1>Hello, Priyank!</h1>
+      {/* <button onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={btnClicked}>Click here!</button> */}
+      {/* <button>onClick={()=>{
+        console.log("Button clicked!");
+      }}</button> */}
+
+    <div>
+      {/* <input onChange={inputChanging} onClick={()=>{
+        console.log("Input pe click hua hai");
+      }} type="text" placeholder='Enter Name' /> */}
+
+        <input 
+        onChange={function(elem){
+          inputChanging(elem.target.value);
+        }} type="text" placeholder='Enter text here' />
+
+    </div>
+
+    </div>
+  )
+}
+export default App;
