67cedc87532177a2bc40b6dcaf523e5ce442285c
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-04-01 15:14:03 +0530

    learn structures

--- Learning C++/structures_unions_enums.cpp
+++ Learning C++/structures_unions_enums.cpp
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <iostream>
 using namespace std;
+
+// struct employee
+// {
+//     /* data */
+//     int eID;
+//     char favChar;
+//     float salary;
+// };
+
+// int main(){
+//     struct employee priyank;
+//     priyank.eID = 01;
+//     priyank.favChar = 'p';
+//     priyank.salary = 120000;
+
+//     cout<<"The Id is " << priyank.eID << endl;
+//     cout<<"The fav character is " << priyank.favChar << endl;
+//     cout<<"The Salary is " << priyank.salary << endl;
+//     return 0;
+// }
+
+//By Using Typedef
+typedef struct employee
+{
+    /* data */
+    int eID;
+    char favChar;
+    float salary;
+} emp;
+
 int main(){
-    
+    emp priyank;
+    priyank.eID = 01;
+    priyank.favChar = 'p';
+    priyank.salary = 120000;
+
+    cout<<"The Id is " << priyank.eID << endl;
+    cout<<"The fav character is " << priyank.favChar << endl;
+    cout<<"The Salary is " << priyank.salary << endl;
     return 0;
 }
