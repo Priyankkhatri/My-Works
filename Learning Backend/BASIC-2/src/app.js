5fc4b6daeaacbaa45e0b6010672de270801f47a6
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-06-09 17:42:21 +0530

    add basic 3 folder for learning mongoose

--- Learning Backend/BASIC-2/src/app.js
+++ Learning Backend/BASIC-2/src/app.js
@@ -3,16 +3,53 @@ const express = require("express");
 
 
 const app = express()
-
+app.use(express.json())
 
 const notes = []
 /* title, description */
 /* POST /notes  */
 app.post("/notes", (req, res)=>{
-    console.log(req.body)
+    notes.push(req.body)
+
+    res.status(201).json({
+        message:"note created successfully!"
+    })
+})
+
+/* GET /notes*/
+app.get('/notes', (req, res) =>{
+    res.status(201).json({
+        message: "notes fetched successfully",
+        notes : notes
+    })
 })
 
 
+
+/* delete /notes/:index/1 */
+
+app.delete('/notes/:index', (req, res)=>{                        //COLON IS TO GET TO KNOW THTA THE INDEX NO. WILL BE DYNAMIC
+    const index = req.params.index    /* 1 */
+    delete notes[ index ]
+
+    res.status(200).json({
+        message:"note deleted successfully!"
+    })
+})
+
+
+
+app.patch('/notes/:index', (req,res)=>{
+    const index = req.params.index
+    const title = req.body.title
+    const description = req.body.description
+
+    notes[ index ].title = title
+    notes[ index ].description = description
+    res.status(200).json({
+        message: "note updatd succcessfully!"
+    })
+})
 
 
 module.exports = app
