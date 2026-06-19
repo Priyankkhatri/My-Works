c687bd2328849dbfcf57e24e7763b6ce4164c9f3
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-06-19 19:39:32 +0530

    used interpolation for displaying data from component class into html template

--- Learning Angular/my-app/src/app/app.ts
+++ Learning Angular/my-app/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  name = "Priyank";
+  age = 19;
+  college = "CodingGita";
 }
