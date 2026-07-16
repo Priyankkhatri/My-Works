3d0340a94ddcf24c517cbdf06ac10a47f0ed8039
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-07-16 18:00:45 +0530

    updated

--- Learning Backend/Part-3/server.js
+++ Learning Backend/Part-3/server.js
@@ -1,7 +1,8 @@
 // server ko start krna
-const app = require("./app");
+const app = require("./src/app");
+const connectDB = require("./src/db/db");
 
-
+connectDB();
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
