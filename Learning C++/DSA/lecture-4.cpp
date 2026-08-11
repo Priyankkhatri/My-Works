d0b3ea7d64d92266edf4e5e7fc638c1529a13532
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-11 16:51:45 +0530

    pattern printing

--- Learning C++/DSA/lecture-4.cpp
+++ Learning C++/DSA/lecture-4.cpp
@@ -84,3 +84,30 @@
 
 
 
+// star pattern question
+// *
+// **
+// ***
+// ****
+// *****
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int row = 1;
+    int count = 1;
+
+    while(row<=n){
+        int col = 1;
+        while(col <= row){
+            cout << "*";
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+    return 0;
+}
