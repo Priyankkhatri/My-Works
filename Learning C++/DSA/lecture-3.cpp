54a8e38930341d0ab3465e2c19f1e78adc22a67c
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-11 09:45:21 +0530

    complete lecture 3

--- Learning C++/DSA/lecture-3.cpp
+++ Learning C++/DSA/lecture-3.cpp
@@ -216,34 +216,4 @@ int main(){
 //     return 0;
 // }
 
-
-
-
-
-// 1234
-// 1234
-// 1234
-// 1234
-
-#include <iostream>
-using namespace std;
-
-int main(){
-
-    int n ;
-    cin >> n;
-
-
-    int i = 1;
-    while(i<=n){
-        int j=1;
-        while(j<=n){
-            cout << j;
-            j++;
-        }
-        cout<<endl;
-        i++;
-    }
-
-    return 0;
-}
+// -------------------Lecture 3 Patterns part-1 completed-------------------------
