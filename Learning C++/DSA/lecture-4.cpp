7b8731f1a715ad2bcc3e8070d9035a179fa6d71e
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-11 14:07:34 +0530

    started lecture-4

--- Learning C++/DSA/lecture-4.cpp
+++ Learning C++/DSA/lecture-4.cpp
@@ -32,24 +32,55 @@
 // 4 3 2 1
 // 4 3 2 1
 // 4 3 2 1
+// #include <iostream>
+// using namespace std;
 
-#include <iostream>
-using namespace std;
+// int main(){
 
-int main(){
+//     int n;
+//     cin >> n;
 
-    int n;
-    cin >> n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout << n-j+1;
+//             j++;
+//         }
+//         cout<< endl;
+//         i++;
+//     }
+//     return 0;
+// }
 
-    int i=1;
-    while(i<=n){
-        int j=1;
-        while(j<=n){
-            cout << n-j+1;
-            j++;
-        }
-        cout<< endl;
-        i++;
-    }
-    return 0;
-}
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     int i=1;
+//     int count = 1;
+
+//     while (i<=n){
+//         int j = 1;
+//         while(j<=n){
+//             cout << count << " ";
+//             count++;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+    
+
+//     return 0; 
+// }
+
+
+
