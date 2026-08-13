7220e4556dede4b55734efe831b02206f098c651
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-13 13:49:54 +0530

    solving pattern questions

--- Learning C++/DSA/lecture-4.cpp
+++ Learning C++/DSA/lecture-4.cpp
@@ -175,24 +175,71 @@
 // 4 5 6
 // 7 8 9 10
 
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     int row = 1;
+//     int count = 1;
+//     while(row<=n){
+//     int col = 1;
+//         while(col <= row){
+//             cout << count << " ";
+//             count++;
+//             col++;
+//         }        
+//         cout << endl;
+//         row++;
+//     }
+//     return 0;
+// }
+
+
+// 1
+// 2 3
+// 3 4 5
+// 4 5 6 7
+
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int n;
-    cin>>n;
-
+    cin >> n;
     int row = 1;
-    int count = 1;
-    while(row<=n){
-    int col = 1;
+    while (row <= n){
+        int col = 1;
+        int value = row;
         while(col <= row){
-            cout << count << " ";
-            count++;
+            cout << value;
             col++;
-        }        
+            value++;
+        }
         cout << endl;
         row++;
     }
+
     return 0;
 }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+//     int n;
+//     cin >> n;
+
+//     int value=1;
+//     for(int row=1; row<=n; row++){
+//         for(int col=1; col<=row;){
+//             cout << value << " ";
+//             value++
+//             col++;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
