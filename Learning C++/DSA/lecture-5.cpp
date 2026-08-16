27ad6f6aa01a8e9e79928938244aa427b29fb819
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-16 10:56:41 +0530

    Bitwise operator , pre, post increment/decrement completed

--- Learning C++/DSA/lecture-5.cpp
+++ Learning C++/DSA/lecture-5.cpp
@@ -9,5 +9,17 @@ int main(){
     cout << " a | b = " << (a|b) << endl;
     cout << " ~ a = " << ~a << endl;
     cout << " a & b = " << (a^b) << endl;
+
+
+
+    int i = 7;
+    cout << (++i) << endl;
+    //8
+    cout << (i++) << endl;
+    //8 , i =9;
+    cout << (i--) << endl;
+    //9, i = 8
+    cout << (--i) << endl;
+    // 7, i=7
     return 0;
 }
