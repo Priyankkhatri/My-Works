baa56b69d08f8518d316516772bc59ebd1235de0
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-16 11:49:43 +0530

    For loop questions

--- Learning C++/DSA/lecture-5.cpp
+++ Learning C++/DSA/lecture-5.cpp
@@ -43,22 +43,42 @@
 // }
 
 //sum of numbers from 1 to n;
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
-    int n;
+// int main() {
+//     int n;
 
-    cout << "Enter the value of n: " << endl;
-    cin >> n;
+//     cout << "Enter the value of n: " << endl;
+//     cin >> n;
 
-    int sum = 0;
+//     int sum = 0;
 
-    for (int i = 1; i <= n; i++) {
-        sum += i;
-    }
+//     for (int i = 1; i <= n; i++) {
+//         sum += i;
+//     }
 
-    cout << "Total sum: " << sum << endl;
+//     cout << "Total sum: " << sum << endl;
 
-    return 0;
-}
+//     return 0;
+// }
+
+// --------------Fibonacci Series-------------
+// 0,1,1,2,3,4,5,8,13,21..........
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n = 10;
+
+//     int a = 0;
+//     int b = 1;
+//     for(int i = 1; i <=n; i++){
+//         int nextNumber = a + b;
+//         cout << nextNumber<<" " << endl; //next numbner is basically a sum
+//         a = b;
+//         b = nextNumber;
+//     }
+
+//     return 0;
+// }
