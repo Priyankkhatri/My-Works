c6e6bc9be1fefbc64550ba5034145d1b10d623bc
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-16 11:20:51 +0530

    For loop questions

--- Learning C++/DSA/lecture-5.cpp
+++ Learning C++/DSA/lecture-5.cpp
@@ -41,3 +41,24 @@
 //     }
 //     return 0;
 // }
+
+//sum of numbers from 1 to n;
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the value of n: " << endl;
+    cin >> n;
+
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    cout << "Total sum: " << sum << endl;
+
+    return 0;
+}
