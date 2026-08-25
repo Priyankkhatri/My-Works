113430045057c7cad8c7ce7a32b8eb72f90f9a95
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-25 15:41:37 +0530

    added next.js folders and routes

---
+++ Learning Next.js/my-app/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const aboutPage = () => {
+  return (
+    <div>This is About Page.</div>
+  )
+}
+
+export default aboutPage
---
+++ Learning Next.js/my-app/src/app/products/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const products = () => {
+  return (
+    <div>This is Products Page</div>
+  )
+}
+
+export default products
